9e266ed493747c264b39bfe0ff27a9171a519eaa
mergeycl <cloe.mergey@reseau.eseo.fr>
2025-07-10 12:26:48 +0200

    test

--- app/public/wp-content/themes/cotemer/footer.php
+++ app/public/wp-content/themes/cotemer/footer.php
@@ -1,1 +1,13 @@
-<?php
+<footer>
+  <div class="footer-section">
+    <h3>Contact</h3>
+    <p>20 RUE DU GENERAL DE GAULLE</p>
+    <p>56640 ARZON</p>
+    <p>France</p>
+    <p>📞 +33 2 97 53 63 67</p>
+  </div>
+
+  <div class="footer-bottom">
+    <p>© 2025 Côté Mer – Tous droits réservés</p>
+  </div>
+</footer>
