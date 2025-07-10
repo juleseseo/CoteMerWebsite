3344321b4983f1e15ec2759c93b7f5704f4ad15d
Jules Poyer <jules.poyer@reseau.eseo.fr>
2025-07-10 15:20:41 +0200

    header

--- app/public/wp-content/themes/cotemer/header.php
+++ app/public/wp-content/themes/cotemer/header.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/assets/img/logo.png" type="image/png">
+    <title>Restaurant Côté Mer</title>
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
@@ -12,9 +14,7 @@
 
         <!-- Logo -->
         <div class="logo">
-            <a href="#accueil">
                 <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.png" alt="Logo du restaurant">
-            </a>
         </div>
 
         <!-- Burger icon (mobile) -->
@@ -28,9 +28,10 @@
         <nav class="main-nav" id="main-nav">
             <ul class="nav-list">
                 <li><a href="#accueil">Accueil</a></li>
-                <li><a href="#about">Présentation</a> </li>
+                <li><a href="#presentation">Présentation</a> </li>
                 <li><a href="#menu">Menu</a></li>
                 <li><a href="#galerie">Galerie</a></li>
+                <li><a href="#about">A propos</a></li>
                 <li><a href="#contact">Contact</a></li>
             </ul>
         </nav>
